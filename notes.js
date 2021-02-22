d6c196a08b6761a5749db9eaccc4aaf3940a6008
Marjorievanderborght <web2020.marjorie@gmail.com>
2021-02-22 18:22:58 +0100

    ajout getters/setters

--- notes.js
+++ notes.js
@@ -1,5 +1,6 @@
 // Dans l'array toDo un valeur true signifie que cette action est accomplie, une valeur false signifie qu'elle est à faire
-let notesJSON = [
+//JSON TJS chaîne de caractères, pas directement un objet
+let notesJSON = `[
 	{
 		"titre": "Notes Javascript",
 		"texte": "Une méthode est une fonction appartenant à un objet",
@@ -17,60 +18,77 @@ let notesJSON = [
 		"couleur": "rouge",
 		"dateRappel": "null"
 	}
-]
-class Note{
-    static nbrInstances = 0;
-    static debug = true;
-    constructor(titre,dateCreation,couleur,dateRappel){
-        Note.nbrInstances++
+]`
+class Note {
+    constructor(titre, dateCreation, couleur, dateRappel = undefined) {
 
-        if(typeof titre === "string") this._titre = titre;
+        if (typeof titre === "string") this._titre = titre;
 
-        if(dateCreation instanceof Date) this._dateCreation = dateCreation;
+        if (typeof dateCreation === "string") this._dateCreation = dateCreation;
 
-        if(["Rouge","Orange","Jaune","Vert"].includes(couleur.toLowerCase())) this._couleur = couleur.toLowerCase();
+        if (["rouge", "orange", "jaune", "vert"].includes(couleur.toLowerCase())) this._couleur = couleur.toLowerCase();
 
-        if(dateRappel instanceof Date) this._dateRappel = dateRappel;
+        if (typeof dateRappel === "string") this._dateRappel = dateRappel;
     }
 
-    get titre(){
+    get titre() {
         return this._titre;
     }
-    get dateCreation(){
+    get dateCreation() {
         return this._dateCreation;
     }
-    get couleur(){
+    get couleur() {
         return this._couleur;
     }
-    get dateRappel(){
+    get dateRappel() {
         return this._dateRappel;
     }
-    set titre(titre){
-        if(typeof titre === "string") {
-            if (Note.debug === true) console.log(this, `titre: ${this.titre} => ${titre}`);
+    set titre(titre) {
+        if (typeof titre === "string") {
             this._titre = titre;
         }
     }
-    set dateCreation(dateCreation){
-        if(dateCreation instanceof Date) {
-            if (Note.debug === true) console.log(this, `dateCreation: ${this.dateCreation} => ${dateCreation}`);
+    set dateCreation(dateCreation) {
+        if (typeof dateCreation === "string") {
             this._dateCreation = dateCreation;
         }
     }
-    set couleur(couleur){
-        if(["Rouge", "Orange", "Jaune","Vert"].includes(couleur.toLowerCase())) {
-            if (Note.debug === true) console.log(this, `couleur: ${this.couleur} => ${couleur}`);
+    set couleur(couleur) {
+        if (["rouge", "orange", "jaune", "vert"].includes(couleur.toLowerCase())) {
             this._couleur = couleur.toLowerCase();
         }
     }
-
-    static validationCouleur(couleurRouge,couleurOrange,couleurJaune,couleurVerte ) {
-        if(couleurRouge.couleur === "Rouge" ||
-           couleurOrange.couleur === "Orange" ||
-           couleurJaune.couleur === "Jaune" ||
-           couleurVerte.couleur === "Vert") {
-            return [couleurRouge,couleurOrange,couleurJaune,couleurVerte];
+    set dateRappel(dateRappel) {
+        if (typeof dateRappel === "string") {
+            this._dateRappel = dateRappel;
         }
-        return false;
+    }
+}
+class TextNote extends Note {
+    constructor(titre, texte, dateCreation, couleur, dateRappel = undefined) {
+        super(titre, dateCreation, couleur, dateRappel);
+        if (typeof texte === "string") this._texte = texte;
+    }
+    get texte() {
+        return this._texte;
+    }
+    set texte(texte) {
+        if (typeof texte === "string") {
+            this._texte = texte;
+        }
+    }
+}
+class Checklistnote extends Note {
+    constructor(titre, toDo = [], dateCreation, couleur, dateRappel = undefined) {
+        super(titre, dateCreation, couleur, dateRappel);
+        this._toDo = toDo;
+    }
+    get toDo() {
+        return this._toDo;
+    }
+    set toDo(toDo) {
+        if (typeof toDo === "string") {
+            this._toDo = toDo;
+        }
     }
 }
